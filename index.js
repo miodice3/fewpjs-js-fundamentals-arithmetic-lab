52dc3d1ea9a517ebbf85658c06c580abc35c5a8f
Michael Iodice <miodice3@gmail.com>
2021-01-15 12:56:58 -0700

    commit tree ff660819ae296d4eb65f55cf9ae25fd3b97ca874

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let test = "JS working"
+
+const num1 = 31
+const num2 = 2
+const multiply = num1*num2
+const random = Number.parseInt(Math.random()*10)+1
+const mod = (num2+num2)%num1
+const max = (1, 2, 3, 4, 20)
